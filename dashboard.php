ca3c14133cf0fdfdd7c9e4a681f8f7f6fd7606ab
alexandre_hamed <alexandrehamed@hotmail.fr>
2017-02-24 08:51:35 +0100

    z

--- dashboard.php
+++ dashboard.php
@@ -6,8 +6,34 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link type="text/css" rel="stylesheet" href="../css/bootstrap.css">
 </head>
 <body>
-<h1> dashboard</h1>
+<div class="container text-center">
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h1>Creer un hero</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4 col-md-offset-3">
+            <a href="#"><button class="btn btn-lg btn-success">creer</button></a>
+        </div>
+        <div class="col-md-6"></div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h1>Creer un monstre</h1>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h1>Creer une carte</h1>
+        </div>
+    </div>
+
+</div>
 </body>
 </html>
